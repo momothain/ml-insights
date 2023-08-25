39ceb13ce93e16fe0a4cf35af213bc6e42385767
Morgann Thain <morgann@instalily.ai>
2023-08-25 12:40:04 -0400

    .env template

--- App.py
+++ App.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///media.db'  # Using SQLite for simplicity
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'
 db = SQLAlchemy(app)
+
 
 class Media(db.Model):
     id = db.Column(db.Integer, primary_key=True)
